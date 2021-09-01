cddfadd15b3fba18edba85215d9334cc8a9dd346
Musodzhon Zakirov <i.zulayho@gmail.com>
2021-09-01 16:46:46 +0500

    Добавьте файлы проекта.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите ключ 'pro' или 'expert'");
+            ProDocumentWorker dc = new ExpertDocumentWorker();
+            dc.key = Console.ReadLine();
+            dc.OpenDocument();
+            dc.EditDocument();
+            dc.SaveDocument();
+            Console.ReadLine();
+        }
+    }
+    class DocumentWorker
+    {
+        public string key = null;
+        public void OpenDocument()
+        {
+            Console.WriteLine("Документ открыт");
+        }
+        public virtual void EditDocument()
+        {
+            Console.WriteLine("Редактирование документа доступно в версии Про");
+        }
+        public virtual void SaveDocument()
+        {
+            Console.WriteLine("Сохранение документа доступно в версии Про");
+        }
+    }
+    class ProDocumentWorker : DocumentWorker
+    {
+        public override void EditDocument()
+        {
+            if (key == "pro" || key == "expert") Console.WriteLine("Документ отредактирован");
+            else base.EditDocument();
+        }
+        public override void SaveDocument()
+        {
+            if (key == "pro") Console.WriteLine("Документ сохранен в старом формате, сохранение в остальных форматах доступно в версии Эксперт");
+            else base.SaveDocument();
+        }
+    }
+    class ExpertDocumentWorker : ProDocumentWorker
+    {
+        public override void SaveDocument()
+        {
+            if (key == "expert") Console.WriteLine("Документ сохранен в новом формате");
+            else base.SaveDocument();
+        }
+    }
+}
